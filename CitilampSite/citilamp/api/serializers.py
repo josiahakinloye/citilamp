094fd7452b8f55af8c9ec6f8094ed7ee0395ddac
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-11-05 20:15:26 +0100

    edited .gitingore

--- CitilampSite/citilamp/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models import *
-class ContinentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Continent
-        fields = ()

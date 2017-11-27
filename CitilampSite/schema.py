8755f63ad13ae4013df1c6bdf090fec9c87282e3
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-11-28 00:49:25 +0100

    Finished with the currency exchange feature

--- CitilampSite/schema.py
+++ CitilampSite/schema.py
@@ -1,13 +1,20 @@
 import graphene
 
-import citilamp.schema as citilampschema
 import AdsSystem.schema as AdsSystemSchema
+import citilamp.schema as citilampschema
+from .exchange import convertCurrency
 
 
 class Query(citilampschema.Query, AdsSystemSchema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to this project
-    pass
+    convert_currency = graphene.Int(currency_from=graphene.String(), currency_to=graphene.String(), quantity=graphene.Int())
+
+    def resolve_convert_currency(self, info, *args, **kwargs):
+        currency_from =kwargs.get('currency_from')
+        currency_to = kwargs.get('currency_to')
+        quantity = kwargs.get('quantity')
+        return convertCurrency(currency_from, currency_to, quantity)
 
 
 schema = graphene.Schema(query=Query)

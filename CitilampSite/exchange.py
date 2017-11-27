daac314b7289790a59e72dc0c17594a49f945d0d
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-11-28 00:55:01 +0100

    fixup! Finished with the currency exchange feature

---
+++ CitilampSite/exchange.py
@@ -0,0 +1,31 @@
+"""
+This module contains everything that has to do with currency exchange
+Tip: to get a list of valid short names of currencies visit https://free.currencyconverterapi.com/
+and look at the select element on their web page or call this api https://free.currencyconverterapi.com/api/v5/currencies
+"""
+
+import requests
+
+
+def convertCurrency(currency_from , currency_to, quantity):
+    """
+    Converts a given currency to another using the value gotten from an api call
+    to api_url variable.
+    :param currency_from: type String The short name of the currency you are  converting from
+    :param currency_to: type String The short of name of the currency you are converting to
+    :param quantity: type Int the quantity of the currency_from  you want to convert
+    :return: type Int the value of the quantity when converted to the currency specified by the param currency_to
+    """
+    currency_from = str(currency_from).upper()
+    currency_to = str(currency_to).upper()
+    api_url = "https://free.currencyconverterapi.com/api/v5/convert?q=" + currency_from+"_"+currency_to +"&compact=y"
+    res  = requests.get(api_url).json()
+    convert_key= currency_from+"_"+currency_to
+    try:
+        current_rate = res[convert_key]['val']
+    except:
+        raise Exception("Api call was unsuccessful")
+    return current_rate * quantity
+
+if __name__  == "__main__":
+    print(convertCurrency("USD","NGN", 1))

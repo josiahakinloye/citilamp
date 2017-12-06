89bab75276218874518bbc6858b34eb96db72935
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-12-06 22:49:15 +0100

    Got slug to me automagically generated

--- blog/admin.py
+++ blog/admin.py
@@ -6,12 +6,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+@admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
 	list_display = ["title", "updated", "timestamp"]
 	list_filter = ["updated", "timestamp"]
 	search_fields = ["title", "content"]
-	class Meta:
-		model = Post
-
-
-admin.site.register(Post, PostModelAdmin)
+	exclude = ['slug',]
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from .views import (
 	post_list,
@@ -8,6 +7,5 @@ from .views import (
 
 urlpatterns = [
 	url(r'^$', post_list, name='list'),
-    #url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/$', PostDetailView.as_view(), name='detail'),
 ]

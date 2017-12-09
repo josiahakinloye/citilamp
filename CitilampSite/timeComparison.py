75f0ab92e5d01241ab93f00d75659e29235727b9
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-12-09 22:53:11 +0100

    --fixup=b69eafea4288de172b4e4a6f3d8e0c12a04c3e41

---
+++ CitilampSite/timeComparison.py
@@ -0,0 +1,58 @@
+import arrow
+import googlemaps
+
+
+#todo change google map credentials
+
+google_maps_key = "AIzaSyCX35UUsEn5Q2duK_j674X-dSp-Xng5W_E"
+gmaps = googlemaps.Client(key=google_maps_key)
+
+def get_latitude_and_longitude(place):
+    """
+    Get the  latitude and longitude  of a place using the google maps api
+    :param place: the place on earth to use eg: lagos, nigeria...
+    :type place str
+    :return: dict containing latitude_and_longitude of place
+    """
+    response = gmaps.geocode(place)
+    try:
+        latitude_and_longitude = response[0]['geometry']['location']
+    except:
+        raise Exception("Could not determine latitude and longitude")
+
+    return latitude_and_longitude
+
+def get_timedetails_of_location(place):
+    """
+    Get the current time at a place
+    :param place: the name of place to get current time of
+    :type place str
+    :return: tuple containing  current date and time of place
+    """
+    details = {}
+    latitude_and_longitude = get_latitude_and_longitude(place)
+    try:
+        timezone_id = gmaps.timezone(latitude_and_longitude)['timeZoneId']
+    except:
+        raise Exception("Can not determine timezone of place")
+    time_details = arrow.now(timezone_id).format('D/MMM/YY-h:mm A')
+    date, time = time_details.split('-')
+
+    details[place]=  (date, time)
+
+    return details
+
+def time_details_comparison(places):
+    """
+    Compare time details in places
+    :param places: iter of strings where each string is a place
+    :type places list, dict or tuple containing list of place to do comparison for
+    :return: list of comparison details
+    """
+    comparison_list = []
+    for place in places:
+        comparison_list.append(get_timedetails_of_location(place))
+    return comparison_list
+
+if __name__ == "__main__":
+    print(time_details_comparison(['lagos', 'germany']))

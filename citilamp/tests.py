934a021ad7d26d2a8b4f31c40267aa99f288aab4
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-01-02 12:13:26 +0100

    sample test for citilamp app

--- citilamp/tests.py
+++ citilamp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import CountryAndCityAttractions
 # Create your tests here.
+
+#todo : remove primrary keys in models
+class citilampTestCase(TestCase):
+    def test_attractions_has_city_or_country(self):
+        test_attraction = CountryAndCityAttractions(name="Test Attraction")
+        with self.assertRaises(Exception) as attraction_error:
+            test_attraction.save()
+            self.assertEqual(attraction_error.msg, "Attraction must be linked to a country or a city")

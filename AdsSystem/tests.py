82e47994adf1a9e82ff7beefedec019a4fc8db1c
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-01-02 11:52:14 +0100

    Tests for ads system

--- AdsSystem/tests.py
+++ AdsSystem/tests.py
@@ -1,16 +1,34 @@
 from datetime import date, timedelta
 
 from django.test import TestCase
-from . models import Ads
+from . models import Ads, AdsError
 
 class AdsTestCase(TestCase):
     """
     This class tests the test of the Ads model
     """
-    def test_create_ad(self):
-        Ads.objects.create(title='Test',description='test', stop_date= date.today()+timedelta(days=1),
-                           owner_name='test', owner_phone_number ='1234567890' ,
-                          owner_email='test@test.com'
-                        )
+    def setUp(self):
+        self.test_ad = Ads(title='Test', description='test',
+                     owner_name='test', owner_phone_number='1234567890',
+                     owner_email='test@test.com'
+                     )
 
+    def test_create_ad(self):
+        self.test_ad.stop_date  = date.today()+timedelta(days=1)
+        self.test_ad.save()
         self.assertQuerysetEqual(Ads.objects.all(), ['<Ads: Test by test>'])
+
+
+    def test_cannot_save_with_bad_stop_date(self):
+        self.test_ad.stop_date = date.today() - timedelta(days=1)
+        with self.assertRaises(AdsError) as save_error:
+            self.test_ad.save()
+            self.assertEqual(save_error.msg, "Ensure start date is not less than today and stop date is greater than start date")
+
+
+    def test_cannot_save_with_bad_start_date(self):
+        self.test_ad.start_date = date.today() - timedelta(days=1)
+        self.test_ad.stop_date = date.today() + timedelta(days=1)
+        with self.assertRaises(AdsError) as save_error:
+            self.test_ad.save()
+            self.assertEqual(save_error.msg, "Ensure start date is not less than today and stop date is greater than start date")

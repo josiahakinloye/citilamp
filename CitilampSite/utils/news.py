3e7162fcd2ad17a9b09865da27a43b5487421972
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-01-18 14:22:05 +0100

    news for country

--- CitilampSite/utils/news.py
+++ CitilampSite/utils/news.py
@@ -2,12 +2,26 @@
 Module for anything relating to news
 """
 import requests
+apiKey = 'cf20aa0d287d4f39bbaac6880c56522b'
+base_headline_url= 'https://newsapi.org/v2/top-headlines?{}&apikey='+apiKey
+
 
 class NewsApiError(Exception):
     """
     Class for handling errors relating to calling the news api
     """
     pass
+
+
+def get_news_for_country(country_code):
+    news_for_country_url = base_headline_url.format('country='+country_code)
+    response = requests.get(news_for_country_url).json()
+    try:
+        news = response['articles']
+    except KeyError:
+        raise NewsApiError("Something went wrong with the news api, could not get news for country with code {}".format(country_code))
+    country_news = map(get_some_news_info, news)
+    return country_news
 
 
 def get_some_news_info(headline_news):
@@ -29,20 +43,26 @@ def get_headline_news():
     """
     Get headline news from google news
     using google news since it is a news aggregator(so you do not need to get news
-    from other sources)
+    from other sources
 
     :return: map object that contains 10 headlines
     """
-    news_api = ('https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=cf20aa0d287d4f39bbaac6880c56522b')
-    response = requests.get(news_api).json()
+    headline_url = base_headline_url.format('sources=google-news')
+    response = requests.get(headline_url).json()
     try:
         headlines = response['articles']
-        headline_news = map(get_some_news_info, headlines)
-        return headline_news
     except:
         raise NewsApiError("Something went wrong with the news api")
+    headline_news = map(get_some_news_info, headlines)
+    return headline_news
 
 if __name__ == "__main__":
     headline_news = get_headline_news()
     for headline in headline_news:
         print(headline)
+        print("\n")
+    country_news= get_news_for_country('ng')
+    print ("News for country")
+    for news in country_news:
+        print(news)
+        print("\n")

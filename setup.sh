1efbc3cc940927efd2c542ba657d06283b76b2a6
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-01-19 23:54:34 +0100

    changed order of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-pip install -r requirements.txt
-sudo apt install git 
+sudo apt install git
 git clone git@github.com:Ilozuluchris/apixu-python.git
 python apixu-python/setup.py install
+pip install -r requirements.txt

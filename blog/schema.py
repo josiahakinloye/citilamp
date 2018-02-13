db7f8391f6061014d60cdd58bd538cf908d2bcef
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-02-13 14:36:05 +0100

    API for getting a particular post

--- blog/schema.py
+++ blog/schema.py
@@ -5,6 +5,7 @@ from graphene_django.types import DjangoObjectType
 
 from .models import Post
 from .views import searched_posts
+from django.contrib.auth.models import User
 
 
 class PostType(DjangoObjectType):
@@ -12,14 +13,28 @@ class PostType(DjangoObjectType):
         model =Post
         exclude_fields = ["image",]
 
+class AuthorType(DjangoObjectType):
+    class Meta:
+        model = User
+
 
 class Query(object):
+    post = graphene.Field(PostType, slug=graphene.String())
     all_posts = graphene.List(PostType)
     query_posts = graphene.List(PostType,query_param=graphene.String())
 
+    all_authors = graphene.List(AuthorType)
+
+    def resolve_post(self, info, *args, **kwargs):
+        slug = kwargs.get('slug')
+        return Post.objects.active().get(slug=slug)
+
     def resolve_all_posts(self, info, *args, **kwargs):
-        return Post.objects.active()
+        return Post.objects.active().select_related('author')
 
     def resolve_query_posts(self, info, *args, **kwargs):
         parameter_to_query_with = kwargs.get("query_param")
         return searched_posts(parameter_to_query_with)
+
+    def resolve_all_authors(self, info, *args, **kwargs):
+        return User.objects.all()
